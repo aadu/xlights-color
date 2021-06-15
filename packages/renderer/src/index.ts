a289f1e38b40da101173f2a13a86a7926987b9a3
Aaron Duke <aaron@0ptimus.com>
2021-06-15 14:16:30 -0700

    add primevue

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,7 +1,13 @@
 import {createApp} from 'vue';
 import App from '/@/App.vue';
 import router from '/@/router';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 createApp(App)
   .use(router)
+  .use(PrimeVue, {ripple: true})
   .mount('#app');
--- scripts/watch.js
+++ scripts/watch.js
@@ -69,7 +69,7 @@ const setupMainPackageWatcher = (viteDevServer) => {
         spawnProcess = null;
       }
 
-      spawnProcess = spawn(String(electronPath), ['.']);
+      spawnProcess = spawn(String(electronPath), ['.', '--no-sandbox']);
 
       spawnProcess.stdout.on('data', d => d.toString().trim() && logger.warn(d.toString(), {timestamp: true}));
       spawnProcess.stderr.on('data', d => d.toString().trim() && logger.error(d.toString(), {timestamp: true}));
