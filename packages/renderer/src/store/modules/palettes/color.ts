1a33a0b3f442fe51d767db94eb3425ea851887d5
Aaron Duke <aaron@0ptimus.com>
2021-06-18 15:41:15 -0700

    add gradient parsing

--- packages/renderer/src/store/modules/palettes/color.ts
+++ packages/renderer/src/store/modules/palettes/color.ts
@@ -1,16 +1,47 @@
+import chroma from 'chroma-js';
+
 let id = 0;
 
 export class Color {
   value: string;
   id: number;
+  isGradient: boolean;
+  isValid: boolean;
+  stops: Array<[string, number]>;
 
   constructor(value: string) {
     id++;
-    this.value = value;
     this.id = id;
+    this.isValid = false;
+    if (chroma.valid(value)) {
+      this.value = value;
+      this.isGradient = false;
+      this.isValid = true;
+    } else {
+      const result = value.match(/(?<=Values=)([^|]+)/);
+      if (result) {
+        this.stops = result[0].split(';')
+          .map(stop => {
+            const re = stop.match(/(?<=x=)(?<location>[^^]+)\^c=(?<color>.*)/);
+            if (re && re.groups) {
+              return [re.groups.color, parseFloat(re.groups.location)]
+            }
+            return null
+          })
+          .filter(_ => _);
+        if (this.stops.length == 1) {
+          this.value = this.stops[0][0];
+          this.isGradient = false;
+          this.isValid = true;
+        } else if (this.stops.length) {
+          this.value = this.stops[0][0];
+          this.isGradient = true;
+          this.isValid = true;
+        }
+      }
+    }
   }
 }
-
 
 export function parsePalette(contents: string) {
   const colors = contents.trimEnd().split(/,/);
